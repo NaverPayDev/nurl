d0a90396f6d90e8b0a8c537f14bbe2c8cecffe32
yongholeeme <i.am.yongho@navercorp.com>
2024-09-04 13:58:25 +0900

    💡 punycode/ import 에 대한 설명 추가

--- src/nurl.ts
+++ src/nurl.ts
@@ -1,3 +1,10 @@
+/**
+ * Note: The trailing slash in the import path 'punycode/' is intentional.
+ * It ensures that this third-party module is used instead of the built-in
+ * Node.js 'punycode' module, which has been deprecated since Node.js v7.0.0.
+ * @see https://github.com/mathiasbynens/punycode.js#installation
+ * @see https://nodejs.org/api/punycode.html for deprecation info
+ */
 import {decode} from 'punycode/'
 
 import {refinePathnameWithQuery, refineQueryWithPathname} from './utils'
